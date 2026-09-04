e6c45509a914928b0c2a4f1a7c240eb41b467e9e
bandijashwanth999-arch <bandijashwanth444@gmail.com>
2026-09-04 14:21:57 +0530

    identity_operator

---
+++ identity_operater.py
@@ -0,0 +1,10 @@
+#identity operator execise
+a=[1,2,3]
+b=a
+result=b is a # is identity opeator
+print("result of",b,"is",a,"is:",result)
+
+c=[1,2,3] 
+d=[1,2,3] 
+result=c is d # identity operator
+print("result of",c,"is",d,"is:",result)
